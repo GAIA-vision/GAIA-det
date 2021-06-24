4a3ec94375085887041e2240302752b9e38c1afc
JrPeng <jrpeng4ever@126.com>
2021-06-24 15:45:10 +0800

    Update ar50to101v2_ft2e_rules.py

--- configs/_dynamic_/rules/ar50to101v2_ft2e_rules.py
+++ configs/_dynamic_/rules/ar50to101v2_ft2e_rules.py
@@ -1,7 +1,6 @@
 model_space_filename = '/path/to/metrics.json'
 
 model_sampling_rules = dict(
-    # NOTE: there are two strategies, pre-metric or post-metric, check which is better
     type='sequential',
     rules=[
         # 1. first 50% mAP models
